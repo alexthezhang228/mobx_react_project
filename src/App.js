3968bf9e46117a0972980872c123f0c210ffad1c
alexthezhang228 <110424337+alexthezhang228@users.noreply.github.com>
2023-04-15 18:11:49 +0200

    add files in src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
+ * @Date: 2023-04-15 17:59:25
+ * @LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
+ * @LastEditTime: 2023-04-15 18:09:26
+ * @FilePath: /mobx/project/myapp/src/App.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
+ * @Date: 2023-04-15 17:59:25
+ * @LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
+ * @LastEditTime: 2023-04-15 18:01:12
+ * @FilePath: /mobx/project/myapp/src/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +22,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
