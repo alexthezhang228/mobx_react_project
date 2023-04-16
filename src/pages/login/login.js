71b5493b8e0b0ec73264881e1c7e8167ebffe788
alexthezhang228 <110424337+alexthezhang228@users.noreply.github.com>
2023-04-16 09:57:13 +0200

    add a form to the login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,18 +2,85 @@
  * @Author: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
  * @Date: 2023-04-15 18:22:51
  * @LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
- * @LastEditTime: 2023-04-15 22:35:33
+ * @LastEditTime: 2023-04-16 09:54:36
  * @FilePath: /mobx/project/myapp/src/pages/login/login.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Card } from "antd"
+import { Card,Form,Input,Checkbox,Button } from "antd"
 import logo from '../../assets/bg.jpg'
 import './style.scss'
 function Login(){
   return (
     <div className="login">
       <Card className="login-container">
-        {/* <img className="login-logo" src={logo} alt=''></img> */}
+        <h1>Please log in</h1>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          // onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        
       </Card>
     </div>
   )
