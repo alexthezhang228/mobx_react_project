7f5218559b0f601536f3237aa093e49173271da7
alexthezhang228 <110424337+alexthezhang228@users.noreply.github.com>
2023-04-16 10:26:59 +0200

    add validation for the form

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,7 +2,7 @@
  * @Author: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
  * @Date: 2023-04-15 18:22:51
  * @LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
- * @LastEditTime: 2023-04-16 09:54:36
+ * @LastEditTime: 2023-04-16 10:25:01
  * @FilePath: /mobx/project/myapp/src/pages/login/login.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,11 +10,19 @@ import { Card,Form,Input,Checkbox,Button } from "antd"
 import logo from '../../assets/bg.jpg'
 import './style.scss'
 function Login(){
+  function onFinish(values){
+    // collect all the data from input in the form
+    console.log(values)
+  }
+  function onFinishFailed(error){
+    console.log(error)
+  }
   return (
     <div className="login">
       <Card className="login-container">
         <h1>Please log in</h1>
         <Form
+          validateTrigger={['onBlur']}
           name="basic"
           labelCol={{
             span: 8,
@@ -28,8 +36,8 @@ function Login(){
           initialValues={{
             remember: true,
           }}
-          // onFinish={onFinish}
-          // onFinishFailed={onFinishFailed}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
           <Form.Item
@@ -40,6 +48,11 @@ function Login(){
                 required: true,
                 message: 'Please input your username!',
               },
+              {
+                pattern:/^\d{10}$/,
+                message:'10 digits tel number',
+                validateTrigger:'onBlur'
+              }
             ]}
           >
             <Input />
